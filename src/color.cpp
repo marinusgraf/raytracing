69d2e2cd2cdfcfe9fb9f5a6d156c8e91d2574ace
Marinus Graf <marinus.graf@outlook.com>
2025-08-07 19:24:27 +0200

    fix

--- src/color.cpp
+++ src/color.cpp
@@ -4,6 +4,11 @@ void write_color(std::ostream& out, const color& pixel_color) {
   auto r = pixel_color.x();
   auto g = pixel_color.y();
   auto b = pixel_color.z();
+
+  r = linear_to_gamma(r);
+  g = linear_to_gamma(g);
+  b = linear_to_gamma(b);
+
   static const interval intensity(0.0, 0.999);
   int ir = int(intensity.clamp(r) * 256);
   int ig = int(intensity.clamp(g) * 256);
--- src/color.hpp
+++ src/color.hpp
@@ -5,6 +5,13 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+  if (linear_component > 0) {
+    return std::sqrt(linear_component);
+  }
+  return 0.0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color);
 
 #endif
